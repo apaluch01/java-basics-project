696a23e4768bab8b2955aa67bb6bf9370331d834
apaluch <apaluch@griddynamics.com>
2022-12-02 07:13:52 +0100

    Main class finished

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import reporting.ReportGenerator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        ReportGenerator generator;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter date for report generation in YYYY-MM-DD HH:MM format");
+        String reportDate = input.nextLine();
+
+        System.out.println("Enter 0 or press enter for a short report version");
+        String reportType = input.nextLine();
+
+        generator = new ReportGenerator(reportDate, reportType);
+        generator.getReport().stream().forEach(r -> System.out.println(r));
+    }
+}
