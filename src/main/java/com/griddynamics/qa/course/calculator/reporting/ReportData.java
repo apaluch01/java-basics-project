fb126b61a56a4d41d4ccd6f7e3b597f545c1c546
apaluch <apaluch@griddynamics.com>
2022-12-09 05:33:23 +0100

    cleared unused import

--- src/main/java/com/griddynamics/qa/course/calculator/reporting/ReportData.java
+++ src/main/java/com/griddynamics/qa/course/calculator/reporting/ReportData.java
@@ -12,7 +12,6 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 public class ReportData {
